0ab11f5cb575ee159f091318ed8b0da9f3ffc12c
sunny yadav <sunny.in645@gmail.com>
2025-10-28 14:07:41 +0530

    deployment settings added

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,38 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
+
 import authRoutes from "./routes/auth.routes.js";
 import movieRoutes from "./routes/movie.routes.js";
 import searchRoutes from "./routes/search.routes.js";
 import tvRoutes from "./routes/tv.routes.js";
+
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 import { protectRoute } from "./middlware/protectRoute.js";
+
 const app = express();
 
-app.use(express.json()); // will allow us to parse req.body
-app.use(cookieParser());
 const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
-app.listen(5000, () => {
-  console.log("Server started at http://localhost:" + PORT);
+
+if (ENV_VARS.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT || 5000, () => {
+  console.log(`✅ Server started on port ${PORT}`);
   connectDB();
 });
